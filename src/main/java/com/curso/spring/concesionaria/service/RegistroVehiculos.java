2bfc811b4ea35544042778d65d2b4c110a33efe8
Junior Ricardo <neo.junior.16@gmail.com>
2019-11-11 19:06:49 -0300

    Detalles css, agregando vehiculos a la lista

--- src/main/java/com/curso/spring/concesionaria/service/RegistroVehiculos.java
+++ src/main/java/com/curso/spring/concesionaria/service/RegistroVehiculos.java
@@ -4,25 +4,35 @@ import java.util.HashMap;
 
 public class RegistroVehiculos {
 
-    private static HashMap<Integer,Vehiculo> miRegistro;
+    private static HashMap<Integer, Vehiculo> miRegistro;
     private static int ultimoId;
-    
-    static{
+
+    static {
         miRegistro = new HashMap<>();
-        miRegistro.put(1, new Vehiculo("Volkswagen","Gol",200));
-        miRegistro.put(2,new Vehiculo("Ford","Fiesta Kinetic",219));
-        ultimoId = 2;
+        miRegistro.put(1, new Vehiculo("Volkswagen", "Amarok", 1500000));
+        miRegistro.put(2, new Vehiculo("Volkswagen", "Vento", 1380000));
+        miRegistro.put(5, new Vehiculo("Volkswagen", "Polo", 932000));
+        miRegistro.put(6, new Vehiculo("FIAT", "500", 1000000));
+        miRegistro.put(7, new Vehiculo("FIAT", "Toro", 1240000));
+        miRegistro.put(8, new Vehiculo("FIAT", "Palio", 700000));
+        miRegistro.put(9, new Vehiculo("FIAT", "Punto", 600000));
+        miRegistro.put(10, new Vehiculo("Citroen", "DS3", 2450000));
+        miRegistro.put(11, new Vehiculo("Citroen", "Aircross", 917000));
+        miRegistro.put(12, new Vehiculo("Citroen", "Cactus", 984000));
+        miRegistro.put(13, new Vehiculo("Peugeot", "5008", 243500));
+        miRegistro.put(14, new Vehiculo("Peugeot", "208", 700000));
+        miRegistro.put(15, new Vehiculo("Peugeot", "408", 845000));
+        miRegistro.put(16, new Vehiculo("Peugeot", "301", 755000));
+        ultimoId = 16;
     }
 
-    public static void setVehiculo(Vehiculo enVehiculo){
+    public static void setVehiculo(Vehiculo enVehiculo) {
         ultimoId++;
         miRegistro.put(ultimoId, enVehiculo);
     }
 
-
-    public static HashMap<Integer,Vehiculo> getTabla(){
+    public static HashMap<Integer, Vehiculo> getTabla() {
         return miRegistro;
     }
 
 }
-
